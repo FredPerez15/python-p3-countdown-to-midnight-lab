0eeeaae5b5fd3ae7b7c74b325c5cc8e1f786373f
Freddy Perez <fred.perez15@gmail.com>
2023-04-04 00:56:49 +0300

    midnight lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+from time import sleep 
+
+def countdown(integer):
+    x = integer
+    while x > 0:
+        print(f'{x} SECOND(S)!')
+        x -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(integer):
+    i = integer
+    while i > 0:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+        sleep(1)
+    print("HAPPY NEW YEAR!")
